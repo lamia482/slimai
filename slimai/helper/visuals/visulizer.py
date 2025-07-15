5928e61b9f2548a68a9be9e27bffbf146aa09733
lamia <qiang.wang@kfbio.cn>
2025-07-15 11:25:33 +0800

    update milvisualizer

--- slimai/helper/visuals/visulizer.py
+++ slimai/helper/visuals/visulizer.py
@@ -1,7 +1,9 @@
 from abc import abstractmethod
 from typing import Dict, Any, List
+import concurrent.futures
+from functools import partial
 from ..utils.select import recursive_select
-import mmengine
+from ..help_utils import ProgressBar, get_dist_env
 
 
 class Visualizer(object):
@@ -13,18 +15,29 @@ class Visualizer(object):
                           targets: Dict[str, Any], 
                           class_names: List[str], 
                           progress_bar: bool = False):
-    vis_list = []
-
-    indices = list(range(len(images)))
     if progress_bar:
-      indices = mmengine.track_iter_progress(indices)
-  
-    for index in indices:
+      pbar = ProgressBar(len(images), desc="Visualizing")
+    
+    def process_single_sample(index, images, outputs, targets, class_names):
       image = recursive_select(images, index)
       output = recursive_select(outputs, index)
       target: dict = recursive_select(targets, index) # type: ignore
-
-      vis = self._visualize(image, target, output, class_names)
-
-      vis_list.append(vis)
+      if progress_bar:
+        pbar.update(sep="\r\t")
+      return self._visualize(image, target, output, class_names)
+    
+    indices = list(range(len(images)))
+    
+    # Create a partial function with fixed arguments
+    process_func = partial(process_single_sample, 
+                          images=images, 
+                          outputs=outputs, 
+                          targets=targets, 
+                          class_names=class_names)
+    
+    # Use ThreadPoolExecutor for parallel processing
+    with concurrent.futures.ThreadPoolExecutor(
+      max_workers=get_dist_env().local_world_size) as executor:
+      vis_list = list(executor.map(process_func, indices))
+      
     return vis_list
